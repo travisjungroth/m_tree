e72fa312515c831987050f632b98526243437703
Travis Jungroth <jungroth@gmail.com>
2022-12-18 16:04:11 -0800

    Mess

--- mtree3.py
+++ mtree3.py
@@ -2,25 +2,44 @@ from __future__ import annotations
 
 import heapq
 import random
+from collections import deque
 from collections.abc import Iterable, Sequence
-from itertools import chain
+from functools import partial, cache
+from itertools import chain, combinations, permutations
 from operator import itemgetter
+from time import sleep
 from typing import Optional, Callable, Generic, TypeVar, Union
+
+import editdistance
 
 Value = TypeVar("Value")
 Distance = int
 
 
+slow = set()
+counter = [0]
+
 def _default_distance(a, b):
-    return abs(a - b)
+    if hash(a) > hash(b):
+        a, b = b, a
+    return _f(a, b)
+
+@cache
+def _f(a, b):
+    counter[0] += 1
+    return editdistance.eval(a, b)
+
+# @cache
+# def u(s):
+#     return sum(ord(c) * i for i, c in enumerate(s, start=1))
 
 
 class MTree(Generic[Value]):
     def __init__(
-        self,
-        values: Iterable[Value],
-        distance_fn: Callable[[Value, Value], Distance] = _default_distance,
-        capacity=100,
+            self,
+            values: Iterable[Value],
+            distance_fn: Callable[[Value, Value], Distance] = _default_distance,
+            capacity=100,
     ):
         self.distance_fn = distance_fn
         self.capacity = capacity
@@ -35,7 +54,7 @@ class MTree(Generic[Value]):
                 children=[ValueNode(value, self)], tree=self, leaf=True
             )
         else:
-            self.root = self.root.insert(value)
+            self.root.insert(value)
         self.size += 1
 
     def __len__(self) -> int:
@@ -47,44 +66,117 @@ class MTree(Generic[Value]):
     def __repr__(self):
         return repr(self.root)
 
-    def knn(self, Q: Value, k: int) -> list[Value]:
+    def __iter__(self):
+        yield from self.root
+
+    def knn(self, value: Value, k: int) -> list[Value]:
         """
         http://www-db.deis.unibo.it/research/papers/SEBD97.pdf
         """
-        k = min(self.size, k)
-        # pq for active subtrees. (min distance from Q for all members, Node)
-        # min distance is max(0, node.distance(Q) - node.radius)
-        PR = [(self.root.min_distance(Q), self.root)]
+        if k >= self.size:
+            return sorted(self, key=partial(self.distance_fn, value))
+        results = _KNNResults(k)
+        pq = PriorityQueue()
+        pq.push(self.root.min_distance(value), self.root)
+        while pq:
+            node: RouterNode
+            min_distance_q, node = pq.pop()
+            results.discard(node)
+            if min_distance_q > results.max_distance():
+                break
+            for child_node in node.children:
+                if abs(node.distance(value) - node.distance(child_node.router)) - child_node.radius <= results.max_distance():
+                    if child_node.min_distance(value) <= results.max_distance():
+                        if isinstance(child_node, RouterNode):
+                            pq.push(child_node.min_distance(value), child_node)
+                        results.push(child_node.max_distance(value), child_node)
+        return results.values()
 
-        # result
-        NN = Res(k)
-
-        return NN.items
-
-    def closest(self, value):
+    def closest(self, value: Value) -> Value:
         distance, leader = self.root.closest(
             value, self.root.distance(value), self.root
         )
         return leader
 
+    def closest_iter2(self, value: Value) -> Value:
+        max_distance = float('inf')
+        q = deque([self.root])
+        closest_value = self.root.router
+        while q:
+            parent_node = q.pop()
+            if parent_node.min_distance(value) >= max_distance:
+                continue
+            for node in parent_node.children:
+                if isinstance(node, RouterNode):
+                    q.appendleft(node)
+                child_max = node.max_distance(value)
+                if child_max < max_distance:
+                    max_distance = child_max
+                    closest_value = node.router
+        return closest_value
 
-class Res:
-    def __init__(self, k: int):
-        self.k = k
-        # negative distance, insertion count, value
-        self.items: list[tuple[Distance, int, Value]] = []
-        self.count = 0
 
-    def push(self, value: Value, distance: Distance) -> Distance:
-        add = -distance, self.count, value
-        if len(self.items) < self.k:
-            heapq.heappush(self.items, add)
-            item = self.items[0]
-        else:
-            item = heapq.heappushpop(self.items, add)
+class PriorityQueue:
+    def __init__(self) -> None:
+        self.items: list[tuple[Distance, int, Node]] = []
+        self.count = 0  # incrementing tie-breaker for comparisons
+
+    def __bool__(self):
+        return bool(self.items)
+
+    def push(self, distance: Distance, node: Node) -> None:
+        new = distance, self.count, node
+        heapq.heappush(self.items, new)
         self.count += 1
-        max_distance = -item[0]
-        return max_distance
+
+    def pop(self) -> tuple[Distance, Node]:
+        priority, count, value = heapq.heappop(self.items)
+        return priority, value
+
+
+class _KNNResults:
+    def __init__(self, k: int) -> None:
+        self.pq: list[tuple[Distance, int, Node]] = []
+        self.count = 0  # incrementing tie-breaker for comparisons
+        self.k = k
+        self.set: set[Node] = set()
+
+    def __len__(self):
+        return len(self.set)
+
+    def push(self, distance: Distance, node: Node) -> None:
+        """
+        Assumes node not in queue
+        """
+        if distance > self.max_distance():
+            return
+        item = -distance, self.count, node
+        self.count += 1
+        self.set.add(node)
+        heapq.heappush(self.pq, item)
+        if len(self) > self.k:
+            self.pop()
+
+    def pop(self):
+        self.clear()
+        _, _, node = heapq.heappop(self.pq)
+        self.set.remove(node)
+        self.clear()
+
+    def clear(self):
+        if not self.pq:
+            return
+        while self.pq[0][2] not in self.set:
+            heapq.heappop(self.pq)
+
+    def discard(self, node: Node) -> None:
+        self.set.discard(node)
+
+    def max_distance(self) -> Distance:
+        return -self.pq[0][0] if self.pq else float('inf')
+
+    def values(self) -> list:
+        return [item[2].router for item in self.pq if isinstance(item[2], ValueNode)]
 
 
 class Node(Generic[Value]):
@@ -105,7 +197,9 @@ class Node(Generic[Value]):
 
 class ValueNode(Node[Value]):
     def __repr__(self):
-        return f"<{repr(self.router)}>"
+        # return f"<{hash(self.router)}>"
+        # return f"<{repr(self.router)}>"
+        return str(id(self))
 
     def __contains__(self, item: Value) -> bool:
         return item == self.router
@@ -120,12 +214,21 @@ class RouterNode(Node[Value]):
         self.is_leaf: bool = leaf
         self.parent: Optional[RouterNode] = None
         self.radius: Distance = 0
-        self.router: Value = None
+        self.router: Value = children[0].router
         self.children: list[Node] = []
-        self._set_children(children)
+        for child in children:
+            self.children.append(child)
+            self.radius = max(self.radius, child.max_distance(self.router))
+            child.parent = self
+
+    def __hash__(self):
+        return hash(id(self))
 
     def __repr__(self):
-        return f"<{repr(self.router)}:{self.radius}, {[(x.router, x.radius) for x in self.children]}>"
+        # return f"<{repr(self.router)}:{self.radius}, {[(x.router, x.radius) for x in self.children]}>"
+        # return f"<{hash(self.router)}:{self.radius}, {[(hash(x.router), x.radius) for x in self.children]}>"
+        return f"<{str(id(self.router))}:{self.radius}, {[(str(id(x.router)), x.radius) for x in self.children]}>"
+
 
     def __iter__(self):
         for child in self.children:
@@ -134,75 +237,65 @@ class RouterNode(Node[Value]):
     def __contains__(self, value: Value) -> bool:
         distance = self.distance(value)
         return distance == 0 or (
-            self.radius >= distance and any(value in child for child in self.children)
+                self.radius >= distance and any(value in child for child in self.children)
         )
-
-    def closest(self, value: Value, distance: Distance, leader: Value) -> tuple[Distance, Value]:
-        if self.is_leaf:
-            # Rewrite to use min, distance as a key, then just compare
-            # to leader.
-            x = chain(
-                [(x.distance(value), x.router) for x in self.children],
-                [(distance, leader)],
-            )
-            return min(x, key=itemgetter(0))
-        for node in self.children:
-            if node.distance(value) - node.radius > distance:
-                continue
-            node: RouterNode
-            distance, leader = min(
-                (distance, leader), node.closest(value, distance, leader)
-            )
-        return distance, leader
 
     def calculate_radius(self, node: Node) -> Distance:
         return max(self.radius, node.max_distance(self.router))
 
-    def insert(self, value: Value) -> RouterNode:
+    def insert(self, value: Value, parent: Optional[Node] = None):
         if self.is_leaf:
-            return self._add_child(ValueNode(value, self.tree))
+            self.add_node(ValueNode(value, self.tree))
+        else:
+            # distances = [(child, child.distance(value)) for child in self.children]
+            # child, distance = min(distances, key=itemgetter(1))
+            # if distance >= child.radius:
+            #     child, _ = min(distances, key=lambda x: x[1] - x[0].radius)
+            # child: RouterNode
+            child = random.choice(self.children)
+            child.insert(value)
+        self.radius = max(self.radius, self.distance(value))
 
-        distances = [(child, child.distance(value)) for child in self.children]
-        child, distance = min(distances, key=itemgetter(1))
-        if distance >= child.radius:
-            child, _ = min(distances, key=lambda x: x[1] - x[0].radius)
-        child: RouterNode
-        child.insert(value)
-        self.radius = self.calculate_radius(child)
-        return self.parent or self
-
-    def _add_child(self, node: Node) -> RouterNode:
-        self.children.append(node)
-        self.radius = self.calculate_radius(node)
+    def add_node(self, node: Node):
         node.parent = self
-        if len(self.children) >= self.tree.capacity:
-            self._split()
-        return self.parent or self
-
-    def _set_children(self, children: Sequence[Node]):
-        self.radius = 0
-        self.router = children[0].router
-        self.children.clear()
-        for child in children:
-            self._add_child(child)
-
-    def _split(self) -> RouterNode:
+        if len(self.children) <= self.tree.capacity:
+            self.children.append(node)
+            self.radius = max(self.radius, node.max_distance(self.router))
+            return
         a_list, b_list = self._promote_and_partition()
-        new_node = RouterNode(children=b_list, tree=self.tree, leaf=self.is_leaf)
-        self._set_children(a_list)
-        if self.parent is None:
+        self.radius = 0
+        self.children.clear()
+        self.router = a_list[0].router
+        for a_child in a_list:
+            self.children.append(a_child)
+            self.radius = max(self.radius, node.max_distance(self.router))
+            a_child.parent = self
+
+        b_node = RouterNode(children=b_list, tree=self.tree, leaf=self.is_leaf)
+        if self is self.tree.root:
             self.parent = RouterNode(children=[self], tree=self.tree, leaf=False)
-        self.parent._add_child(new_node)
-        return self.parent
+            self.tree.root = self.parent
+        self.parent.add_node(b_node)
+        self.parent.radius = max(self.parent.radius, self.max_distance(self.parent.router))
 
     def _promote_and_partition(self) -> tuple[Sequence[Value], Sequence[Value]]:
-        # could split out promote and partition and make them customizable
-        random.shuffle(self.children)
-        a, b, *rest = self.children
+        # a, b = max(permutations(self.children, r=2), key=lambda x: self.tree.distance_fn(x[0].router, x[1].router))
+        # a_list, b_list = [], []
+        # nodes = self.children
+
+        # random.shuffle(self.children)
+        a, b, *nodes = self.children
         a_list, b_list = [a], [b]
-        for node in rest:
-            if self.distance(a.router) < self.distance(b.router):
+
+        for node in nodes:
+            if random.random() < 0.5:
+            # if node.distance(a.router) < node.distance(b.router):
                 a_list.append(node)
             else:
                 b_list.append(node)
+
+        # for l in [a_list, b_list]:
+        #     router = min(l, key=lambda x: sum([y.max_distance(x.router) for y in l]))
+        #     i = l.index(router)
+        #     l[0], l[i] = l[i], l[0]
         return a_list, b_list
--- tests.py
+++ tests.py
@@ -1,32 +1,50 @@
 import random
+from heapq import nsmallest
+from string import ascii_letters, ascii_lowercase
 from time import time
 
-from mtree3 import MTree, _default_distance
+from mtree3 import MTree, _default_distance, slow, counter, _f, RouterNode
+
+
+def random_string(n=100):
+    return ''.join(random.choices(ascii_lowercase, k=n))
 
 
 def test_something():
     random.seed(42)
     size = 10 ** 4
-    numbers = random.sample(range(size*10), size)
-    # print(numbers)
-    # for m in [MTree(numbers, capacity=4), set(numbers)]:
-    #     start = time()
-    #     assert -1 not in m
-    #     for n in numbers:
-    #         assert (n in m)
-        #
-        # print(time() - start)
-    start = time()
-    m = MTree(numbers, capacity=8)
-    print(time() - start)
-    start = time()
-    needle = size // 4
-    a = m.closest(needle)
-    print(time() - start)
-    start = time()
-    b = min(numbers, key=lambda x: _default_distance(x, needle))
-    print(time() - start)
-    assert a == b
+    # numbers = [random.random() * size for _ in range(size)]
+    # numbers = random.sample(range(0, size*10, 2), size)
+    print()
+    strings = list({random_string() for _ in range(size)})
+    random.shuffle(strings)
+    for cap in [8]:
+        _f.cache_clear()
+        start = time()
+        m = MTree(strings, capacity=cap)
+        print('tree', time() - start)
+        k = 3
+        for _ in range(1):
+            needle = random_string()
+
+
+            counter[0] = 0
+            start = time()
+            a = sorted(m.knn(needle, k))
+            print(counter, time() - start)
+
+            _f.cache_clear()
+            counter[0] = 0
+            start = time()
+            b = sorted(nsmallest(k, strings, key=lambda x: _default_distance(x, needle)))
+            # b = min(numbers, key=lambda x: _default_distance(x, needle))
+            print(counter, time() - start)
+
+
+            for x in a:
+                d1 = _default_distance(x, needle)
+                d2 = _default_distance(b[-1], needle)
+                assert d1 <= d2
     # start = time()
     # size // 2 in m
     # print(time() - start)
@@ -37,3 +55,25 @@ def test_something():
     #     m.insert(n)
     #     for s in seen:
     #         assert s in m, i
+
+def test_a():
+    size = 10 ** 4
+    # numbers = [random.random() * size for _ in range(size)]
+    # numbers = random.sample(range(0, size*10, 2), size)
+    print()
+    strings = list({random_string(100) for _ in range(size)})
+    random.shuffle(strings)
+    m = MTree(strings, capacity=4)
+
+    stack = [(m.root,)]
+    while stack:
+        path = stack.pop()
+        node = path[-1]
+        for child in node.children:
+            assert child.parent == node
+            if isinstance(child, RouterNode):
+                stack.append(path + (child,))
+            else:
+                for parent in path:
+                    assert parent.distance(child.router) <= parent.radius
+test_a()
